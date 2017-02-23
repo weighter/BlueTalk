77728ca84fa2c4ebc6110a26f94ba39373605159
Weighter <875745369@qq.com>
2017-02-23 16:01:12 +0800

    Initial Commit

---
+++ BlueTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlueTalk
+//
+//  Created by Weighter on 2017/2/23.
+//  Copyright © 2017年 Weighter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlueTalk/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlueTalk
+//
+//  Created by Weighter on 2017/2/23.
+//  Copyright © 2017年 Weighter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
